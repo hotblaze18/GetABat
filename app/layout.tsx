6fc50dfd5d4c2e27215c7a977b576f1eeb23f66e
Pranav <pranav@prjecttinker.in>
2024-06-01 22:41:54 +0530

    Add favicon.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -19,6 +19,11 @@ export default function RootLayout({
          <link rel="preconnect" href="https://fonts.googleapis.com" />
          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={''} />
          <link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,700&family=Silkscreen:wght@400;700&display=swap" rel="stylesheet" />
+         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+         <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-512x512.png" />
+         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+         <link rel="manifest" href="/site.webmanifest" />
          <body>{children}</body>
     </html>
   );
