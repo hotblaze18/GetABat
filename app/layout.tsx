653958ab0c2d42ffaa90f4c0f794ef9ea6f9ba77
Pranav <pranav@prjecttinker.in>
2024-07-28 12:39:27 +0530

    move links elements to head in layout.tsx.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
-import type {Metadata} from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "GETABAT",
-  description: "Top quality sports equipment at affordable prices.",
+  title: 'GETABAT',
+  description: 'Top quality sports equipment at affordable prices.',
 };
-
-
-
 
 export default function RootLayout({
   children,
@@ -15,16 +12,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-         <link rel="preconnect" href="https://fonts.googleapis.com" />
-         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={''} />
-         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet" />
-         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-         <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-512x512.png" />
-         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-         <link rel="manifest" href="/site.webmanifest" />
-         <body>{children}</body>
-    </html>
+    <>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={''} />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&Lora:ital,wght@0,400..700;1,400..700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-512x512.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </>
   );
 }
